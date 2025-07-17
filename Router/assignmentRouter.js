94a784e7d53716c1ab464074010e2299a3db76cd
Ayan2001Mandal <ayanmandal909@gmail.com>
2025-07-17 23:32:41 +0530

    feat: Add viewAssignment controller and update assignmentRouter to include route

--- Router/assignmentRouter.js
+++ Router/assignmentRouter.js
@@ -2,8 +2,10 @@ const express = require('express');
 const assignmentRouter = express.Router();
 const upload = require('../middlewares/uplode');
 const createAssignment  = require('../controllers/assignmentControllers/createAssignment');
+const viewAssignment = require('../controllers/assignmentControllers/viewAssignment');
 const authMiddleware = require('../middlewares/auth');
 
 assignmentRouter.post('/create', authMiddleware, upload.single('file'), createAssignment);
+assignmentRouter.get('/',authMiddleware,viewAssignment);
 
 module.exports = assignmentRouter;
